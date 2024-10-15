d66e05d1b44fced7c04e8289d71223eec09e7a04
Pablo de Rivas <p.derivas@itecriocuarto.org.ar>
2024-10-15 12:05:58 -0300

    Agregar hasheo de contraseñas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,13 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 
 const createUser = async (req, res) => {
   try {
-    const { name, email, password, type } = req.body;
+    let { name, email, password, type } = req.body;
+    
+    password = await bcrypt.hash(password, 10)
+    console.log(password)
     const user = await User.create({ name, email, password, type });
     res.status(201).json(user);
   } catch (error) {
@@ -38,4 +43,27 @@ const getUserById = async (req, res) => {
   }
 };
 
-module.exports = { createUser, getUsers, getUserById };
+const login = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    const user = await User.findAll({
+      where: {
+        email: email
+      }
+    })
+    const hashPassword = user[0].password
+    bcrypt.compare(password, hashPassword).then(res => {
+      console.log(res)
+    })
+    if (!user) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    res.status(200).json({'hola': 'xd'});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error obteniendo el usuario' });
+  }
+}
+
+module.exports = { createUser, getUsers, getUserById, login };
