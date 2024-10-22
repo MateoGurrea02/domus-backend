b07e0bd9103947421d75dd1b7f16cae2a32c9afc
Pablo de Rivas <p.derivas@itecriocuarto.org.ar>
2024-10-21 23:45:58 -0300

    Agregar middleware isAdminOrAgent

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -3,9 +3,10 @@ const { createClient, getClients, getClientById } = require('../controllers/clie
 const router = express.Router();
 const { isAdmin } = require('../middlewares/isAdmin')
 const { isAgent } = require('../middlewares/isAgent')
+const { isAdminOrAgent } = require('../middlewares/isAdminOrAgent')
 
-router.post('/clients', isAgent, createClient);
-router.get('/clients', isAdmin, getClients);
+router.post('/clients', isAdminOrAgent, createClient);
+router.get('/clients', isAdminOrAgent, getClients);
 router.get('/clients/:id', getClientById);
 
 module.exports = router;
