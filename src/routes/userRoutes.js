fb455f7f09627079b0cd886bf8e4e21e3274abc8
Pablo de Rivas <p.derivas@itecriocuarto.org.ar>
2024-10-27 14:37:56 -0300

    Obtener los usuarios accesible por los agentes tambien

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { createUser, getUsers, getUserById, login, verifyToken } = require('../controllers/userController');
 const router = express.Router();
 const { isAdmin } = require('../middlewares/isAdmin')
+const { isAdminOrAgent } = require('../middlewares/isAdminOrAgent')
 
 router.post('/register', createUser);
 router.post('/login', login)
-router.get('/users', isAdmin, getUsers);
-router.get('/users/:id', isAdmin, getUserById);
+router.get('/users', isAdminOrAgent, getUsers);
+router.get('/users/:id', isAdminOrAgent, getUserById);
 router.get('/verifyToken', verifyToken)
 
 module.exports = router;
