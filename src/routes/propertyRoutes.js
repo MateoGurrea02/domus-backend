5bb09b33b801f712ce5fd0d280bff133b0b92646
Pablo de Rivas <p.derivas@itecriocuarto.org.ar>
2024-10-31 12:30:04 -0300

    Agregar ruta de obtener las propiedades de un cliente

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const { createProperty, getProperties, getPropertyById, getPropertiesByAgent, deleteProperty, filterProperty, updateProperty } = require('../controllers/propertyController');
+const { createProperty, getProperties, getPropertyById, getPropertiesByAgent, getPropertiesByClient, deleteProperty, filterProperty, updateProperty } = require('../controllers/propertyController');
 const { uploadImage, getPropertyImages } = require('../controllers/imageController')
 const router = express.Router();
 const { isAgent } = require('../middlewares/isAgent')
@@ -11,6 +11,7 @@ router.post('/properties/filter', filterProperty)
 router.get('/properties', getProperties);
 router.get('/properties/find/:id', getPropertyById);
 router.get('/properties/agent/', isAgent, getPropertiesByAgent);
+router.get('/properties/client/:id', getPropertiesByClient);
 router.put('/properties/update/:id', isAgent, updateProperty)
 router.delete('/properties/:propertyId', isAgent, deleteProperty);
 
